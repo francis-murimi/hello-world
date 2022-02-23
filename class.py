40b3e716a002bfc7000db0e6da281bf96cba4d09
Francis Murimi <76974414+francis-murimi@users.noreply.github.com>
2022-02-23 17:02:17 +0300

    Add car class with two attributes.

--- class.py
+++ class.py
@@ -4,3 +4,11 @@ class Person:
 
 mtu = Person("John Doe")
 print(mtu.name)
+
+class Car:
+    def __init__(self,brand,year):
+        self.brand = brand
+        self.year = year
+
+car = Car('Toyota', '2008')
+print(car.brand + " " + car.year)
