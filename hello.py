719a4e1dad18cad01d639d7575c1ee9374c59735
Francis Murimi <76974414+francis-murimi@users.noreply.github.com>
2022-02-23 15:25:04 +0300

    python scripts

---
+++ hello.py
@@ -0,0 +1,8 @@
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
+print("This is my file to test Python's execution methods.")
+print("The variable __name__ tells me which context this file is running in.")
+print("The value of __name__ is:", repr(__name__))
